23272c5d3fe70f6279fa5c67e8b5f2785aa94827
shiyivei <shiyivei@outlook.com>
2022-06-20 16:47:51 +0800

    uodate contract

--- reward.sol
+++ reward.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
  
-contract TransferERC20Token {
+contract TokenReward {
 
   address public owner;
   uint  balance;
@@ -33,31 +33,26 @@ contract TransferERC20Token {
   //   balance -= amount;
   //   emit TransferSent(msg.sender,destAddr,amount);
   // }
-
-  //
-  function transferERC20(IERC20 token, uint amount, string memory reportHash,address publicAddr) public { 
+  
+  function transferERC20(IERC20 tokenContractAddress, uint amount, string memory reportHash,address receiveAddress) public { 
 
     //verify hash 
-    require(bytes(reportHash).length >0,"invalid report hash, please check it again");
+    require(bytes(reportHash).length >0,"Invalid report hash, please check it again");
 
     //duplicate reward varify
-    require(user_is_reward[reportHash]== false,"the report hash has been used,please try another one ");
+    require(user_is_reward[reportHash]== false,"The report hash has been used,please try another one ");
 
     //Only owner can transfer funds
     require(msg.sender == owner,"Only owner can transfer funds");
 
     //get erc20 token balance
-    uint256 erc20balance = token.balanceOf(address(this));
-    require(amount <= erc20balance,"balance is low");
+    uint256 erc20balance = tokenContractAddress.balanceOf(address(this));
+    require(amount <= erc20balance,"Balance is low");
 
     //transfer
-    token.transfer(publicAddr,amount);
-    emit TransferSent(msg.sender,publicAddr,amount);
+    tokenContractAddress.transfer(receiveAddress,amount);
+    emit TransferSent(msg.sender,receiveAddress,amount);
 
     user_is_reward[reportHash] = true;
   }
-
 }
-
-
- 
